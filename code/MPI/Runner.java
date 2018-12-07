1f13ec59d94879be9c8be5d8bb54abd50ff2cd51
nickrohde <rohden@cwu.edu>
2018-12-07 12:54:53 -0800

    updated the mpi java version

--- code/MPI/Runner.java
+++ code/MPI/Runner.java
@@ -4,9 +4,15 @@ import org.apache.commons.math3.random.RandomGenerator;
 import org.apache.commons.math3.random.RandomDataGenerator;
 import java.lang.Math;
 import mpi.*;
+import org.nustaq.serialization.FSTConfiguration;
+import org.nustaq.serialization.FSTObjectInput;
+import org.nustaq.serialization.FSTObjectOutput;
+import org.nustaq.serialization.util.FSTInputStream;
 
 public class Runner
 {
+    private static FSTConfiguration conf = FSTConfiguration.createDefaultConfiguration();
+
     public static void main(String[] argv)
     {
         MPI.Init(argv);                                               // initialize MPI
@@ -37,6 +43,46 @@ public class Runner
 
         MPI.Finalize();                                               // let MPI know we're done
     } // end Main
+
+
+    private static Solution compare_solutions(Solution x, int my_rank, int mpi_size)
+    {
+        if(my_rank == 0)
+        {
+            Solution res = null;
+
+            for (int src = 1; src < mpi_size; src++)
+            {
+                // get message length from source node
+                int length[] = new int[1];
+                MPI.COMM_WORLD.recv(length, 0, 1, MPI.INT, src, 0);
+
+                // receive actual message
+                byte msg[] = new byte[length[0]];
+                MPI.COMM_WORLD.recv(msg, 0, 1, MPI.BYTE, src, 0);
+
+                // add object to list
+                Solution temp = (Solution)conf.asObject(msg);
+
+                if (res == null || res.energy() > temp.energy())
+                {
+                    res = new Solution(temp);
+                } // end if
+            } // end for
+
+            return res;
+        } // end if
+        else
+        {
+            byte msg = conf.asByteArray(x);
+            int length[] = new int[1];
+            length[0] = msg.length;
+            MPI.COMM_WORLD.send(length, 0, 1, MPI.INT, 0, 0);
+            MPI.COMM_WORLD.send(msg, 0, msg.length, MPI.BYTE, 0, 1);
+
+            return null;
+        } // end else
+    } // end method compare_solutions
 
 
     private static Graph make_graph(String file_name)
