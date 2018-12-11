87d8989a52bec85f280e4aee2a6e938110ee8888
Nasser Al-Ghmadi <Nasser@D-10-156-5-158.dhcp4.washington.edu>
2018-12-10 17:34:32 -0800

    Mass template

---
+++ code/MASS/src/main/java/edu/uwb/sa_mass/Annealing.java
@@ -0,0 +1,130 @@
+/*
+
+ 	MASS Java Software License
+	© 2012-2015 University of Washington
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	The following acknowledgment shall be used where appropriate in publications, presentations, etc.:      
+
+	© 2012-2015 University of Washington. MASS was developed by Computing and Software Systems at University of 
+	Washington Bothell.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+
+*/
+
+package edu.uwb.sa_mass;
+
+import java.net.InetAddress;
+
+import org.apache.commons.math3.random.MersenneTwister;
+import org.apache.commons.math3.random.RandomDataGenerator;
+import org.apache.commons.math3.random.RandomGenerator;
+
+import edu.uw.bothell.css.dsl.MASS.Place;
+
+public class Annealing extends Place {
+
+	// public static final int GET_HOSTNAME = 0;
+	public static final int START_ANNEALING = 0;
+	private RandomDataGenerator rng;
+	private Solution candidate;
+	private int iterations = 0;
+
+	/**
+	 * This constructor will be called upon instantiation by MASS The Object
+	 * supplied MAY be the same object supplied when Places was created
+	 * 
+	 * @param obj
+	 */
+	public Annealing(Object[] args) {
+		RandomGenerator mt19937 = new MersenneTwister(60L * getIndex()[0]); // random engine
+		rng = new RandomDataGenerator(mt19937); // RNG used by annealing
+		candidate = new Solution(60L * getIndex()[0]);
+		candidate.init((Graph) args[0]);
+		// candidate = (Solution) initSolution;
+		this.iterations = (Integer) args[1];
+	}
+
+	/**
+	 * This method is called when "callAll" is invoked from the master node
+	 */
+	public Object callMethod(int method, Object[] o) {
+
+		switch (method) {
+
+		case START_ANNEALING:
+			return runAnnealingProcess((Solution) o[0], (Double) o[1]);
+
+		default:
+			return new String("Unknown Method Number: " + method);
+
+		}
+
+	}
+
+	/**
+	 * Return a String identifying where this Place is actually located
+	 * 
+	 * @param o
+	 * @return The hostname (as a String) where this Place is located
+	 */
+	public Object runAnnealingProcess(Solution o, double heat) {
+
+		try {
+			Solution best = candidate;
+			for (int i = 0; i < this.iterations; i++) {
+				// create a new solution from our current candidate
+				Solution new_solution = new Solution(candidate);
+
+				// find a neighboring solution by swapping a number of random cities
+				new_solution.random_swap(1);
+
+				// we will always accept a better solution
+				if (new_solution.energy() < candidate.energy()) {
+					candidate = new Solution(new_solution);
+
+					// check if this is better than what we've ever seen
+					if (candidate.energy() < best.energy()) {
+						best = new Solution(candidate);
+					} // end if
+				} // end if
+				else {
+					// check if we will accept this new solution
+					if (acceptance_probability(candidate.energy(), new_solution.energy(), heat) > rng.nextUniform(0.0,
+							1.0)) {
+						// new solution was accepted
+						candidate = new Solution(new_solution);
+					} // end if
+				} // end else
+			} // end for i
+
+			return (Solution) best;
+		}
+
+		catch (Exception e) {
+			return "Error : " + e.getLocalizedMessage() + e.getStackTrace();
+		}
+
+	}
+
+	private static double acceptance_probability(double previous_solution, double new_solution, double current_heat) {
+		return (1 / (1 + Math.exp((new_solution - previous_solution) / current_heat)));
+	} // end method acceptance_probability
+
+}
---
+++ code/MASS/src/main/java/edu/uwb/sa_mass/Solution.java
@@ -0,0 +1,192 @@
+package edu.uwb.sa_mass;
+import java.util.Arrays;
+import org.apache.commons.math3.random.MersenneTwister;
+import org.apache.commons.math3.random.RandomGenerator;
+import org.apache.commons.math3.random.RandomDataGenerator;
+import java.text.DecimalFormat;
+
+public class Solution
+{
+    // Static members:
+    private RandomGenerator _mt19937;
+    private RandomDataGenerator _rng;
+
+
+    // Members:
+    private int[] _order;
+    private double _distance;
+    private Graph _network;
+
+
+    // Constructors:
+    public Solution()
+    {
+        _order = null;
+        _distance = -1;
+        _network = null;
+        _mt19937 = new MersenneTwister(0x5d11141018463324L);
+        _rng = new RandomDataGenerator(_mt19937);
+    } // end Default Constructor
+
+    public Solution(long seed)
+    {
+        this();
+        _mt19937 = new MersenneTwister(seed);
+        _rng = new RandomDataGenerator(_mt19937);
+    } // end Constructor(long)
+
+    public Solution(Solution other)
+    {
+        _order = Arrays.copyOf(other._order, other._order.length);
+        _distance = other._distance;
+        _network = new Graph(other._network);
+        _mt19937 = other._mt19937;
+        _rng = other._rng;
+    } // end Copy Constructor
+
+
+    // Class initiate functions:
+    private void init(int[] order, double distance)
+    {
+        _order = Arrays.copyOf(order, order.length);
+        _distance = distance;
+    } // end method init(int[], double)
+
+    public void init(int[] order, Graph network)
+    {
+        // delegate to private init with calculated trip length
+        init(order, calculate_trip_length(order, network));
+        _network = new Graph(network);
+    } // end method init(int[], Graph) 
+
+    public void init(Graph network)
+    {
+        // delegate to init(int[], Graph) with random trip
+        init(gen_random_trip(network), network);
+    } // end method init(Graph) 
+
+
+    // Accessors:
+    public int[] order()
+    {
+        return Arrays.copyOf(_order, _order.length);
+    } // end method order
+    
+    public int length()
+    {
+        return _order.length;
+    } // end method length
+
+    public double energy()
+    {
+        return _distance;
+    } // end method energy
+
+
+    // Mutators:
+    public void random_swap(int num_swaps)
+    {
+        for (int i = 0; i < num_swaps; i++ )
+        {
+            int swap_1 = _rng.nextInt(0, _order.length - 1),
+                swap_2 = _rng.nextInt(0, _order.length - 1),
+                temp = _order[swap_1];
+            _order[swap_1] = _order[swap_2];
+            _order[swap_2] = temp;
+        } // end for i
+
+        _distance = calculate_trip_length(_order, _network);
+    } // end method random_swap
+
+    public void reseed(long seed)
+    {
+        _mt19937.setSeed(seed);
+        _rng = new RandomDataGenerator(_mt19937);
+    } // end method reseed
+
+
+    // Internal utility functions:
+    private double calculate_trip_length(int[] trip, Graph network)
+    {
+        double distance = network.distance_to_origin(trip[0]);
+
+        for (int i = 1; i < trip.length; i++)
+        {
+            // distance between consecutive cities
+            distance += network.get_distance(trip[i-1], trip[i]);
+        } // end for i
+
+        return distance;
+    } // end method calculate_trip_length
+
+    private int[] gen_random_trip(Graph network)
+    {
+        int size = network.size();
+        return _rng.nextPermutation(size, size);
+    } // end method gen_random_trip
+
+
+    // Relational Operators:
+    public boolean equals(Solution other)
+    {
+        boolean out = true;
+
+        for (int i = 0; i < _order.length; i++)
+        {
+            if (_order[i] != other._order[i])
+            {
+                out = false;
+                break;
+            } // end if
+        } // end for
+
+        return out;
+    } // end method equals
+
+
+    // Utility functions:
+    @Override
+    public String toString()
+    {
+        String out = "path: ";
+        for (int i = 0; i < _order.length; i++)
+        {
+            out += _order[i];
+
+            if (i+1 < _order.length)
+            {
+                out += " -> ";
+            } // end if
+            else
+            {
+                out += " | distance: " + _distance;
+            } // end else
+        } // end for i
+
+        return out;
+    } // end method toString(void)
+
+    public String toString(DecimalFormat format)
+    {
+        String out = "path: ";
+        for (int i = 0; i < _order.length; i++)
+        {
+            out += _order[i];
+
+            if (i+1 < _order.length)
+            {
+                out += " -> ";
+            } // end if
+            else
+            {
+                out += " | distance: " + format.format(_distance);
+            } // end else
+        } // end for i
+
+        return out;
+    } // end method toString(DecimalFormat)
+} // end class Solution
+
+
+
+
