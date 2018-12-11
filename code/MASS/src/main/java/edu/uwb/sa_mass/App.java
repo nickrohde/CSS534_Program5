c2f655b0b5db929c568d943c52acae75543f2eee
Anjal Doshi <anjaldoshi09@gmail.com>
2018-12-11 12:48:46 -0800

    made changes to report. refactored MASS files

--- code/MASS/src/main/java/edu/uwb/sa_mass/App.java
+++ code/MASS/src/main/java/edu/uwb/sa_mass/App.java
@@ -1,32 +1,3 @@
-/*
-
- 	MASS Java Software License
-	© 2012-2015 University of Washington
-
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
-
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
-
-	The following acknowledgment shall be used where appropriate in publications, presentations, etc.:      
-
-	© 2012-2015 University of Washington. MASS was developed by Computing and Software Systems at University of 
-	Washington Bothell.
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-	THE SOFTWARE.
-
-*/
 
 package edu.uwb.sa_mass;
 
@@ -54,7 +25,7 @@ public class App {
 	public static void main( String[] args ) {
 
 		int n = Integer.parseInt(args[0]);                          // time to wait during annealing steps for the system to stabilize
-		int num_nodes = Integer.parseInt(args[2]);                          // time to wait during annealing steps for the system to stabilize
+		int num_nodes = Integer.parseInt(args[2]);                  // number of nodes
         Graph g = make_graph(args[1]);                              // input graph file
 
 		int num_places = num_nodes;
@@ -72,26 +43,28 @@ public class App {
 		MASS.getLogger().debug( "MASS library initialized" );
 		
 		/* 
-		 * Create all Places (having dimensions of x, y, and z)
-		 * ( the total number of Place objects that will be created is: x * y * z )
+		 * Create all Places with the same number as the number of nodes
 		 */
 	
-		MASS.getLogger().debug( "Quickstart creating Places..." );
+		MASS.getLogger().debug( "App creating Places..." );
 		//Object[] arguments = new Object[] { g, n/num_nodes};
 		
 		Places places = new Places( 1, Annealing.class.getName(), (Object) new Integer(n/num_nodes), num_places); // creating places
 		MASS.getLogger().debug( "Places created" );
 
+		//Object array to store the arguments for callALL
 		Object[] placeCallAllObjs = new Object[num_places];
 
 		double heat = INITIAL_HEAT;
 		int step = 2;
 
+		//initalize the best solution
 		Solution best = new Solution();
 		best.init(g);
 
 		while (heat > MIN_HEAT)
         {
+			//store arguements for each place
 			for(int i=0; i<placeCallAllObjs.length; i++){
 				Object[] placeArgHelper = new Object[2]; 
 				placeArgHelper[0] = (Object) best;
@@ -99,11 +72,13 @@ public class App {
 				placeCallAllObjs[i] = (Object) new Arg_Helper(placeArgHelper);
 			}
 
+			//Run and store the output of the annealing method for each place
 			Object[] calledPlacesResults = (Object[]) places.callAll(Annealing.START_ANNEALING, placeCallAllObjs);
 
 			double best_energy = ((Solution) calledPlacesResults[0]).energy();
 			int best_index = 0;
 
+			//Select the solution from with the minmum distance from all places
 			for(int j=1; j<calledPlacesResults.length ; j++){
 				if( ((Solution) calledPlacesResults[j]).energy() < best_energy ){
 					best_index = j;
@@ -111,60 +86,19 @@ public class App {
 				}
 			}
 
+			//set the best solution obtained from above as the new best solution
 			best = new Solution( (Solution)calledPlacesResults[best_index] );
-            // update system entropy and annealing step counter
+		   
+			// update system entropy and annealing step counter
             heat = temperature(heat, step);
             step += 1;
         } // end while
 
 		/*
-		// instruct all places to return the hostnames of the machines on which they reside
-		Object[] placeCallAllObjs = new Object[ x * y * z ];
-		MASS.getLogger().debug( "Quickstart sending callAll to Places..." );
-		Object[] calledPlacesResults = ( Object[] ) places.callAll( Matrix.GET_HOSTNAME, placeCallAllObjs );
-		MASS.getLogger().debug( "Places callAll operation complete" );
-		
-		
-
-		// create Agents (number of Agents = x * y in this case), in Places
-		MASS.getLogger().debug( "Quickstart creating Agents..." );
-		Agents agents = new Agents( 1, Nomad.class.getName(), null, places, x * y );
-		MASS.getLogger().debug( "Agents created" );
-
-		// instruct all Agents to return the hostnames of the machines on which they reside
-		Object[] agentsCallAllObjs = new Object[ x * y ];
-		MASS.getLogger().debug( "Quickstart sending callAll to Agents..." );
-		Object[] calledAgentsResults = ( Object[] ) agents.callAll( Nomad.GET_HOSTNAME, agentsCallAllObjs );
-		MASS.getLogger().debug( "Agents callAll operation complete" );
-		
-		// move all Agents across the Z dimension to cover all Places
-		for (int i = 0; i < z; i ++) {
-			
-			// tell Agents to move
-			MASS.getLogger().debug( "Quickstart instructs all Agents to migrate..." );
-			agents.callAll(Nomad.MIGRATE);
-			MASS.getLogger().debug( "Agent migration complete" );
-			
-			// sync all Agent status
-			MASS.getLogger().debug( "Quickstart sending manageAll to Agents..." );
-			agents.manageAll();
-			MASS.getLogger().debug( "Agents manageAll operation complete" );
-			
-			// find out where they live now
-			MASS.getLogger().debug( "Quickstart sending callAll to Agents..." );
-			calledAgentsResults = ( Object[] ) agents.callAll( Nomad.GET_HOSTNAME, agentsCallAllObjs );
-			MASS.getLogger().debug( "Agents callAll operation complete" );
-			
-		}
-		
-		// find out where all of the Agents wound up when all movements complete
-		MASS.getLogger().debug( "Quickstart sending callAll to Agents to get final landing spot..." );
-		calledAgentsResults = ( Object[] ) agents.callAll(Nomad.GET_HOSTNAME, agentsCallAllObjs );
-		MASS.getLogger().debug( "Agents callAll operation complete" );
-		
 		// orderly shutdown
 		MASS.getLogger().debug( "Quickstart instructs MASS library to finish operations..." );
 		*/
+
 		MASS.finish();
 		MASS.getLogger().debug( "MASS library has stopped" );
 		
